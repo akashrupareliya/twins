00eec96528dad7be73e523f72f70eab909f1f31f
Akash Rupareliya <akashrupareliya@users.noreply.github.com>
2016-12-06 04:03:15 -0400

    Update help.java
    added more details in help section

--- BringMe/app/src/main/java/com/twins/bringme/help.java
+++ BringMe/app/src/main/java/com/twins/bringme/help.java
@@ -18,7 +18,7 @@ public class help extends AppCompatActivity {
         //Display Help Screen.
         setContentView(R.layout.help);
         TextView help = (TextView)findViewById(R.id.help_text);
-        String helptext ="<u><b>What is Location Authenticty?</b></u><br>Description of Location Authenticity</br>";
+        String helptext ="<u><b>1.Location Authenticity</b></u><br>This rating is the measure of how correct the store location is, this is calculated based on the user provided ratings</br><br><br><u><b>2.Seller Rating</b></u><br>This rating is the measure of seller popularity.</br><br><br><u><b>3.On-Sale</b></u><br>This is an optional field which indicates whether the item is on-sale or not.</br><br><br><u><b>4.Approx. Quantity </b></u><br>This is an optional field in which user can enter the approximate number of the item available in particular store.</br>";
 
         //To convert HTML text to normal text.
         help.setText(Html.fromHtml(helptext));
